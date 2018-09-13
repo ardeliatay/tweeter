45cfc1386cc5f4879501cd4c6dab20b387d682de
Ardelia Tay <ardeliatay@gmail.com>
2018-09-13 21:01:26 +0000

    updated dependencies to include mongodb

---
+++ mongo_example.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const MongoClient = require("mongodb").MongoClient;
+//can also be written as: const {MongoClient} = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+
+  function getTweets(callback) {
+    //finds tweets and receives it in an array
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+
+    db.close();
+  });
+
+});
+
+
+
+
+
+
+
+
+
